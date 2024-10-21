2a210bd6060268607fffb567e2108bb1f5a273da
Aditya <adityagangwar516@gmail.com>
2024-10-21 17:02:57 +0530

    fixed switch

--- src/routes/(authenticated)/employees_table/displayEmployeeModal.svelte
+++ src/routes/(authenticated)/employees_table/displayEmployeeModal.svelte
@@ -76,7 +76,7 @@
             class:selected={showSummary}
             on:click={handleSummary}
             id="Summary"
-            name="switchTimeline"
+            name="switchTimelines"
             value="Summary"
             checked="checked"
           />
@@ -85,7 +85,7 @@
             class:selected={showTimeline}
             on:click={handleTimeline}
             id="Timeline"
-            name="switchTimeline"
+            name="switchTimelines"
             value="Timeline"
           />
           <label for="Summary">Summary</label>
